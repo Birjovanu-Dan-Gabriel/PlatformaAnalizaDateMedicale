006bc492d01a051ee6d93602d2a9ad87a43b80f7
dan dan <birjovanudan123@gmail.com>
2025-06-06 11:12:50 +0300

    versiune finala

--- R/analysis.R
+++
@@ -1,9 +0,0 @@
-library(ggplot2)
-
-# Încărcare date
-health_data <- read.csv("data/raw_data.csv")
-
-# Analiză rapidă
-summary(health_data)
-table(health_data$region, health_data$smoker)
-ggplot(health_data, aes(x=age, y=bmi, color=gender)) + geom_point()
